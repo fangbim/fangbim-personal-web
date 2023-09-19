0ef851c8db04e9d96f1f60156ae6c7bab0c18fb4
fangbim <fang.bim891@gmail.com>
2023-09-19 08:17:12 +0700

    change background color & disable right click

--- app/components/textReavel.tsx
+++ app/components/textReavel.tsx
@@ -5,21 +5,25 @@ import SplitType from 'split-type';
 import SocialMedia from './socialMedia';
 
 export default function TextReavel() {
-    const text : string = 'Fang.Bim';
-
     useEffect(() => {
         const char = new SplitType('#my-text',);
         gsap.to('.char', {
             y: 0,
-            stagger: 0.05,
-            delay: 0.2,
-            duration: .01
+            stagger: 0.08,
+            duration: .05
         });
+
+        gsap.to('#title', {
+          x: 0,
+          stagger: 0.05,
+          duration: 0.01
+        })
     });
 
   return (
     <div className='flex flex-col justify-center items-center h-screen'>
-        <h1 id='my-text' className='text-8xl md:text-[10rem] clip-path-mpolygon leading-[4.8rem] md:leading-[9rem]'>{text}</h1>
+        <h1 id='my-text' className='text-8xl md:text-[10rem] clip-path-mpolygon leading-[4.8rem] md:leading-[9rem]'>Fang.Bim</h1>
+        <p id='title' className='m-0 text-lg tracking-[0.5rem]'>I&apos;M A Software Engineer</p>
         <SocialMedia/>
     </div>
     
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
+'use client'
+import { useEffect } from "react";
 import TextReavel from "./components/textReavel";
 
 export default function Home() {
+
+  useEffect(() => {
+    document.addEventListener("contextmenu", handleContextMenu)
+    return() => {
+      document.removeEventListener("contextmenu", handleContextMenu)
+    }
+  },[])
+
+  const handleContextMenu = (e : any) => {
+    e.preventDefault();
+  };
   return (
     <main>
         <TextReavel/>
